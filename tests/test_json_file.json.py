73babf4af55bc0777b2ffb9e2036e54e5272eeb4
Лиза Герман <liza36046@gmail.com>
2023-05-17 15:15:43 +0300

    Create README.md

--- tests/test_json_file.json.py
+++ tests/test_json_file.json.py
@@ -1,6 +1,7 @@
+from utils import sort_data
+
 def test_sort_data():
-micto_data_with_date =
-[{
+    micro_data =  [{
     "date": "2023-06-30T02:08:58.425572",
   },
   {
@@ -8,5 +9,15 @@ micto_data_with_date =
   },
   {
     "date": "2019-04-04T23:20:05.206878",
+  }]
+
+    assert sort_data(micro_data) ==  [{
+    "date": "2023-06-30T02:08:58.425572",
+  },
+  {
+    "date": "2019-04-04T23:20:05.206878",
+  },
+  {
+    "date": "2018-03-23T10:45:06.972075",
   }]
 
